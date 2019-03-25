5740806d428e3e6e2d53612ddad6c4a7e896b0c4
jenkinsadmin <rajeshbauit1@gmail.com>
2019-03-25 05:15:03 +0000

    modified script

--- helloworld.sh
+++ helloworld.sh
@@ -2,5 +2,19 @@
 
 echo " this is my first shell script for jenkins"
 echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
+echo " I am gonna execute this in jenkins throgh git !!!!!! "
 
 
